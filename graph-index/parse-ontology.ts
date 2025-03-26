c3ed1c8a538005a90dbdae7b9166ec6bfa97b3d3
istamarahsan <san@sanii.my.id>
2025-03-26 14:03:00 +0700

    feat: ontology prompt fragment

---
+++ graph-index/parse-ontology.ts
@@ -0,0 +1,32 @@
+// deno-lint-ignore-file no-explicit-any
+import * as xmlParse from "@libs/xml/parse"
+import _ from "lodash";
+import { parseArgs } from "@std/cli/parse-args"
+
+if (import.meta.main) {
+    const { f, o } = parseArgs(Deno.args, {
+        string: ["f", "o"],
+        default: {"o": "data/prompt-ontology.txt"}
+    })
+    if (!f) {
+        console.error("Specify an RDF file with --f")
+        Deno.exit(-1);
+    }
+    using ontologyFile = await Deno.open(f)
+    const content = xmlParse.parse(ontologyFile) as any
+    const text = `## Ontology Concepts
+${content["rdf:RDF"]["rdfs:Class"].map(fragmentClass).join("\n\n")}
+
+## Ontology Relations
+${content["rdf:RDF"]["rdf:Property"].map(fragmentProperty).join("\n\n")}
+`
+    await Deno.writeTextFile(o, text)
+}
+
+function fragmentClass(obj: any): string {
+    return `### ${obj["@rdf:about"]}\n${(obj["rdfs:comment"] ?? "").replaceAll("\n", "").replaceAll("\r", "")}`
+}
+
+function fragmentProperty(obj: any): string {
+    return `### ${obj["@rdf:about"]}\nDomain: ${obj["rdfs:domain"]["@rdf:resource"]}\nRange: ${obj["rdfs:range"]["@rdf:resource"]}${"rdfs:comment" in obj ? "\n" + obj["rdfs:comment"].replaceAll("\n", "").replaceAll("\r", "") : ""}`
+}
